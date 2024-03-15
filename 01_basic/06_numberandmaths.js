e43cfaf2128a63e340243bf0be520be138bcb107
vivekpal468 <141908938+vivekpal468@users.noreply.github.com>
2024-03-15 08:28:16 +0000

    numbers

---
+++ 01_basic/06_numberandmaths.js
@@ -0,0 +1,22 @@
+const score = 400
+// console.log(score)
+
+
+const anotherScore=new Number(400)
+// console.log(anotherScore)
+
+// console.log(anotherScore.toString().length);
+// console.log(anotherScore.toFixed(3));
+
+const next = 12.365
+//console.log(next.toPrecision(3));
+
+const nexttonext = 100000
+//console.log(nexttonext.toLocaleString("en-IN"));//by defaults it takes US system
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                //MATHS//
+console.log(Math)
+
