9e05f3fabf55396aa256e0ac1742a556ffbd9f8b
vivekpal468 <141908938+vivekpal468@users.noreply.github.com>
2024-04-02 15:49:40 +0000

    functions

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -22,12 +22,31 @@
 
 // addtwonumbers(56, 89)
 
-function IsloggedIn(username = "sam")
-{
-    if(!username)
-    console.log("please enter a vlid username");
-    else
-    return `${username} is logged in`
-}
+// function IsloggedIn(username = "sam")
+// {
+//     if(!username)
+//     console.log("please enter a vlid username");
+//     else
+//     return `${username} is logged in`
+// }
 
-console.log(IsloggedIn("vivek"));
+// console.log(IsloggedIn("vivek"));
+
+// function calculateCartprice(...num1)
+// {
+//     return num1;
+// }
+// console.log(calculateCartprice(100,200,500,2000));
+
+const user = {
+    username: "vivek pal",
+    price: "399"
+}
+function handleObject(anyObject)
+{
+    console.log(`username is ${anyObject.username} and the price of the product is ${anyObject.price}`);
+}
+handleObject ({
+    username: "sam",
+    price: "999"
+})
